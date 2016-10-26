1547365a792ab56bf1a418c30bdb729128943604
redpepel <90-50@mail.ru>
2016-10-26 13:45:26 +0300

    Update sircle.java

--- com/circle/sircle.java
+++ com/circle/sircle.java
@@ -19,6 +19,15 @@ public class sircle {
         this.y = 0;
 
     }
+    public sircle( double x, double y) {
+        if (radius < 0) {
+            throw new IllegalArgumentException("МЕНЬШЕ");
+        }
+        this.radius = radius;
+
+
+    }
+    
 
 
     public void move(double dx, double dy) {
@@ -26,6 +35,37 @@ public class sircle {
         y = y + dy;
 
 
+    }
+    public double diametr() {
+        double diam = radius * 2;
+        return diam;
+    }
+
+    public double area() {
+        double area = radius * radius * 3.14;
+        return area;
+    }
+
+    public double dlina() {
+        double l = (radius * 2) * 3.14;
+        return l;
+    }
+
+    public void lineika(double dradius) {
+        radius = radius + dradius;
+
+    }
+
+    public String proverka(double tochkax, double tochkay) {
+        double pr = x + radius;
+        double pr1 = y + radius;
+        String true1 = "точка находится в пределах данной окружности";
+        String false1 = "Точка находится за пределами данной окружности";
+        if ((tochkax >= x && tochkax <= pr) && (tochkay >= y && tochkay <= pr1)) {
+            return true1;
+        } else {
+            return false1;
+        }
     }
 
     ;

4f44f17dcdf1c72d0f1a9a8a6cd9a23dd6f96d3a
redpepel <90-50@mail.ru>
2016-10-26 13:28:15 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+import com.circle.sircle;
+import com.krug.krug;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        krug t = new krug(4,0, 0);
+        System.out.println(t.proverka(77,1));
+        System.out.println(t.dlina());
+
+
+
+
+    }
+}
